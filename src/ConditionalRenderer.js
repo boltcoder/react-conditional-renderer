4854696d17700846a16b670f0bb74ec72b971be9
boltcoder <choudharyshrey19@gmail.com>
2017-05-26 14:55:36 +0530

    fixed custom params

--- src/ConditionalRenderer.js
+++ src/ConditionalRenderer.js
@@ -8,27 +8,28 @@ import { connect } from 'react-redux';
  * @param {[type]} options.onConditionSuccess Function post condition success. If not passed component will be rendered as default.
  * @param {[type]} options.onConditionFailure function post condition failure. If not passed component will not be rendered.
  */
-export default function conditionalRenderer(Component, { condition, onConditionSuccess, onConditionFailure }) {
+export default function conditionalRenderer(Component, { condition, onConditionSuccess, onConditionFailure }, customParams) {
   
   class WrapperComponent extends React.Component {
     constructor(props) {
       super(props);
       this.authorize = this.authorize.bind(this);
       this.shouldRender = false;
+      this.customParams = customParams;
     }
     componentWillMount() {
-      this.authorize(this.props);
+      this.authorize(this.props, this.customParams);
     }
-    authorize(props) {
-      if (condition(props)) {
+    authorize(props, customParams) {
+      if (condition(props, customParams)) {
         // if on validation success you may still not want the component to be rendered
-        this.shouldRender = typeof onConditionSuccess === 'function' ? onConditionSuccess(props) : true;
+        this.shouldRender = typeof onConditionSuccess === 'function' ? onConditionSuccess(props, customParams) : true;
         if (this.shouldRender === 'undefined') {
           this.shouldRender = true;
         }
       } else {
         // if on validation failure you may still want the component to be rendered
-        this.shouldRender = typeof onConditionFailure === 'function' ? onConditionFailure(props) : false;
+        this.shouldRender = typeof onConditionFailure === 'function' ? onConditionFailure(props, customParams) : false;
         if (this.shouldRender === 'undefined') {
           this.shouldRender = false;
         }
